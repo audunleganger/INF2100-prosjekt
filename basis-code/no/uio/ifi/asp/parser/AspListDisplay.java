6dceeb70ca6f1abc279d915da77085c0c9c3e78a
Audun Leganger <audun.leganger@gmail.com>
2018-10-04 12:16:07 +0200

    Implemented AspListDisplay

--- basis-code/no/uio/ifi/asp/parser/AspListDisplay.java
+++ basis-code/no/uio/ifi/asp/parser/AspListDisplay.java
@@ -7,18 +7,50 @@ import static no.uio.ifi.asp.scanner.TokenKind.*;
 
 
 class AspListDisplay extends AspAtom{
+    ArrayList<AspExpr> expr = new ArrayList<>();
 
     AspListDisplay(int n){
         super(n);
     }
 
     static AspListDisplay parse(Scanner s){
-        //does nothing now
-        return null;
+        enterParser("ListDisplay");
+
+        AspListDisplay ald = new AspListDisplay(s.curlineNum());
+
+        skip(s, leftBracketToken);
+
+        while(true) {
+            aa.expr.add(AspExpr.parse(s));
+            if (s.curToken().kind != commaToken)    {
+                break;
+            }
+            skip(s, commaToken);
+        }
+
+        skip(s, rightBracketToken);
+
+        leaveParser("ListDisplay");
+
+        return ald;
     }
 
     @Override
     void prettyPrint(){
-        //does nothing now
+        int amount_p = expr.size() - 1;
+        Main.log.prettyWrite(" [");
+        if (expr.size() == 1)   {
+            expr.get(0).prettyPrint();
+        }
+        else    {
+            for (AspExpr ae : expr)  {
+                ae.prettyPrint();
+                if (amount_p != 0)  {
+                    Main.log.prettyWrite(", ");
+                    amount_p--;
+                }
+            }
+        }
+        Main.log.prettyWrite("] ");
     }
 }
