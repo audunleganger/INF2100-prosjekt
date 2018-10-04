945966d48ed4450d3c20a33fc5fa80fa8df9311e
takeolt <paulius476@gmail.com>
2018-10-04 14:38:35 +0200

    AspSuite fixed

--- basis-code/no/uio/ifi/asp/parser/AspSuite.java
+++ basis-code/no/uio/ifi/asp/parser/AspSuite.java
@@ -9,19 +9,43 @@ import no.uio.ifi.asp.runtime.*;
 
 
 class AspSuite extends AspSyntax{
+    ArrayList<AspStmt> stmt = new ArrayList<>();
 
     AspSuite(int n){
         super(n);
     }
 
     static AspSuite parse(Scanner s){
-        //does nothing
-        return null;
+        enterParser("Suite");
+
+        AspSuite as = new AspSuite(s.curLineNum());
+
+        skip(s, newLineToken);
+        skip(s, indentToken);
+
+        while(true){
+            as.stmt.add(AspStmt.parse(s));
+            if(s.curToken().kind == dedentToken){
+                break;
+            }
+        }
+
+        skip(s, dedentToken);
+
+        leaveParser("Suite");
+
+        return as;
+
     }
 
     @Override
     void prettyPrint(){
-        //does nothing
+        Main.log.prettyWriteln("\n");
+        Main.log.prettyIndent();
+        for(AspStmt as : stmt){
+            as.prettyPrint();
+        }
+        Main.log.prettyDedent();
     }
 
     @Override
