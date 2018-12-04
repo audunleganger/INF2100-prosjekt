ef8857d388ca092eea786d8b7c3bf06ddbf45189
takeolt <paulius476@gmail.com>
2018-12-04 10:25:21 +0100

    nesten ferdig

--- basis-code/no/uio/ifi/asp/runtime/RuntimeLibrary.java
+++ basis-code/no/uio/ifi/asp/runtime/RuntimeLibrary.java
@@ -17,7 +17,7 @@ public class RuntimeLibrary extends RuntimeScope {
                 checkNumParams(actualParams, 1, "len", where);
                 return actualParams.get(0).evalLen(where);
             }});
-
+        // print
         assign("print", new RuntimeFunc("print") {
             @Override
             public RuntimeValue evalFuncCall(ArrayList<RuntimeValue> actualParams, AspSyntax where) {
@@ -28,6 +28,84 @@ public class RuntimeLibrary extends RuntimeScope {
                 System.out.println();
                 return new RuntimeNoneValue();
             }});
+
+        //str
+        assign("str", new RuntimeFunc("str") {
+            @Override
+            public RuntimeValue evalFuncCall(ArrayList<RuntimeValue> actualParams, AspSyntax where) {
+                return new RuntimeStringValue(actualParams.get(0).toString());
+            }
+        });
+
+        //float
+        assign("float", new RuntimeFunc("float") {
+            @Override
+            public RuntimeValue evalFuncCall(ArrayList<RuntimeValue> actualParams, AspSyntax where) {
+                if (actualParams.get(0) instanceof RuntimeIntValue) {
+                    return new RuntimeFloatValue(actualParams.get(0).getIntValue("Convert", where));
+                }
+                else if (actualParams.get(0) instanceof  RuntimeFloatValue) {
+                    return actualParams.get(0);
+                }
+                else if (actualParams.get(0) instanceof  RuntimeStringValue) {
+                    double val = Double.parseDouble(actualParams.get(0).toString());
+                    return new RuntimeFloatValue(val);
+                }
+                else {
+                    runtimeError("Expected int/float/string got: " +
+                            actualParams.get(0).typeName(), where);
+                    return null;
+                }
+            }
+        });
+
+        //int
+        assign("int", new RuntimeFunc("int") {
+            @Override
+            public RuntimeValue evalFuncCall(ArrayList<RuntimeValue> actualParams, AspSyntax where) {
+                if (actualParams.get(0) instanceof RuntimeIntValue) {
+                    return actualParams.get(0);
+                }
+                else if (actualParams.get(0) instanceof  RuntimeFloatValue) {
+                    return new RuntimeIntValue((long) actualParams.get(0).getFloatValue("Convert", where));
+                }
+                else if (actualParams.get(0) instanceof  RuntimeStringValue) {
+                    long val = Integer.parseInt(actualParams.get(0).toString());
+                    return new RuntimeIntValue(val);
+                }
+                else {
+                    runtimeError("Expected int/float/string got: " +
+                            actualParams.get(0).typeName(), where);
+                    return null;
+                }
+            }
+        });
+
+        //range
+        assign("range", new RuntimeFunc("range") {
+            @Override
+            public RuntimeValue evalFuncCall(ArrayList<RuntimeValue> actualParams, AspSyntax where) {
+                if(actualParams.size() != 2) {
+                    runtimeError("To little or to many arg in ", where);
+                }
+
+                else if (!(actualParams.get(0) instanceof RuntimeIntValue) ||
+                        !(actualParams.get(1) instanceof RuntimeIntValue)) {
+                    runtimeError("Expected a int got: " + actualParams.get(0).typeName()
+                    + " in line: ", where);
+                }
+
+                ArrayList<RuntimeValue> number_list = new ArrayList<>();
+                int number1 = (int) actualParams.get(0).getIntValue("Range func", where);
+                int number2 = (int) actualParams.get(0).getIntValue("Range func", where);
+
+                for(int i = number1; i<number2; i++) {
+                    number_list.add(new RuntimeIntValue(i));
+                }
+
+                return new RuntimeListValue(number_list);
+            }
+        });
 
     }
 
